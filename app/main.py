a741d0233d35d40fa2d6290a6a4381d41712a3ea
Shan <pruskimateusz93@gmail.com>
2022-07-31 13:03:33 +0200

    Cleanup

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
-#
-# from .routers import game
-#
-# app.include_router(game.router)
-#
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello Bigger Applications!"}
-#
 import uvicorn
-from fastapi import Depends, HTTPException, FastAPI
+from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
 import crud
 from database import Base, SessionLocal, engine
 import schemas
-from gg_deals import scraper
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -32,15 +22,7 @@ def get_db():
 def read_game(game_title: str, db: Session = Depends(get_db)):
     db_game = crud.get_game_by_title(db, game_title=game_title)
     if db_game is None:
-        print("Generating")
         return crud.create_game_by_title(db, game_title)
-        # db_game = scraper.get_game_prices(game_title)
-        # db.add(db_game)
-        # db.commit()
-        # db.refresh(db_game)
-        pass
-        # raise HTTPException(status_code=404, detail="Game not found")
-    print("Found!")
     return db_game
 
 if __name__ == '__main__':
