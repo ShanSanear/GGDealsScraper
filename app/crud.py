3167282a24d4f7ac70428d3d852d7111130a19a4
Shan <pruskimateusz93@gmail.com>
2022-08-14 21:54:31 +0200

    Added updating game prices by title

--- app/crud.py
+++ app/crud.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import sqlalchemy
 from sqlalchemy.orm import Session
 
@@ -30,6 +32,18 @@ def create_game_price(db: Session, game_price: schemas.GamePriceCreate, game_id:
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+def update_game_by_title(db: Session, game_title: str) -> models.Game:
+    game_db_item = get_game_by_title(db, game_title)
+    scrapped_info = scraper.get_game_prices(game_title)
+    prices = [models.GamePrice(**price_info.dict()) for price_info in scrapped_info.prices]
+    game_db_item.prices = prices
+    game_db_item.last_update = datetime.now()
+    db.add(game_db_item)
+    db.commit()
+    db.refresh(game_db_item)
+    return game_db_item
 
 
 def create_game_by_title(db: Session, game_title: str) -> models.Game:
