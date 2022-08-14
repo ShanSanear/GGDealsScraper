12b7c3e7c1ab9debedd0249a7bea405d75af3441
Shan <pruskimateusz93@gmail.com>
2022-08-14 21:55:40 +0200

    Refreshing game listing if older than 7 days

--- app/routers/game.py
+++ app/routers/game.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -13,4 +15,6 @@ def read_game(game_title: str, db: Session = Depends(get_db)):
     db_game = crud.get_game_by_title(db, game_title=game_title)
     if db_game is None:
         return crud.create_game_by_title(db, game_title)
+    elif db_game.last_update is None or (datetime.now() - db_game.last_update) > timedelta(days=7):
+        return crud.update_game_by_title(db, game_title)
     return db_game
